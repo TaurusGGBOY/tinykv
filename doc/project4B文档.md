1258b935f9a4a7509444c195d1c16d78b1fa4b8c
ubuntu2 <854357635@qq.com>
2022-04-01 08:23:38 -0700

    finish 4B

--- doc/project4B文档.md
+++ doc/project4B文档.md
@@ -11,3 +11,54 @@
 + latch key类似key锁
 + 事务使用starttimestamp标志的
 + 处理error
+
+## 思想
+
++ lock：操作某个key的时候必须加锁（包括commit和default），写default之前加锁，commit之后解锁
++ default：加锁，写default列族，返回
++ write：如果commit了，write写入这个key和commitTS，释放锁，注意要加latch进行并发控制
+
+## 函数
+
++ KvGet 
++ KvPrewrite
+  + lock的kind是要根据m.Op进行改变的
+## 测试
+
+### TestGetLocked4B
+
++ Expected :[]byte{0x2a}  Actual   :[]byte(nil)
+  + 我的逻辑是如果有锁就放弃，返回nil
+  + 看结果的意思是，加锁了，应该可以读之前的版本？
+  + 改成不判断锁之后，240行会调用一些锁的属性出 问题
+  + 解决：改成只有请求大于等于锁ts的才报错
+  + 看一下KeyError
+#### 想法
+
++ 想要获取lock GetLock，就要首先创建一个txn
++ NewMvccTxn方法创建txn，需要传入reader
++ reader可以用server.storage.reader
++ reader需要一个上下文，用req里面的上下文……
+
+### TestSinglePrewrite4B
++ bug1:Expected :[]byte{0x1, 0x1,}  Actual   :[]byte(nil)
+  + 还没有实现lock
+  + 事务设置好了要用 server.storage.Write写入
+
+### TestPrewriteLocked4B
+
++ bug1 : Error:      	Not equal: expected: 1  actual  : 0
+  + 没有考虑加锁冲突
+  + 在加锁之前先获取锁
+
+### TestCommitConflictRollback4B
+
++ bug1:assert.False(builder.t, builder.mem.HasChanged(kv.cf, key)) 
+  + HasChanged我返回是true
+  + 要么是result = s.CfWrite.Get(item)==nil
+  + 要么是!result.fresh==true
+  + 解决：在commit的时候要遍历一下锁，如果没有锁，就说明prewrite有问题，直接返回
+
+## 结果
+
+![](https://raw.githubusercontent.com/TaurusGGBOY/photobed/master/20220401230557.png)
--- kv/server/server.go
+++ kv/server/server.go
@@ -2,11 +2,11 @@ package server
 
 import (
 	"context"
-
 	"github.com/pingcap-incubator/tinykv/kv/coprocessor"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/kv/storage/raft_storage"
 	"github.com/pingcap-incubator/tinykv/kv/transaction/latches"
+	"github.com/pingcap-incubator/tinykv/kv/transaction/mvcc"
 	coppb "github.com/pingcap-incubator/tinykv/proto/pkg/coprocessor"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/tinykvpb"
@@ -50,17 +50,133 @@ func (server *Server) Snapshot(stream tinykvpb.TinyKv_SnapshotServer) error {
 // Transactional API.
 func (server *Server) KvGet(_ context.Context, req *kvrpcpb.GetRequest) (*kvrpcpb.GetResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	response := new(kvrpcpb.GetResponse)
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		return response, err
+	}
+	defer reader.Close()
+
+	txn := mvcc.NewMvccTxn(reader, req.Version)
+	lock, err := txn.GetLock(req.Key)
+	if lock != nil && lock.Ts <= req.Version {
+		response.Error = &kvrpcpb.KeyError{
+			Locked: &kvrpcpb.LockInfo{
+				Key:         req.Key,
+				PrimaryLock: lock.Primary,
+				LockVersion: lock.Ts,
+				LockTtl:     lock.Ttl,
+			},
+		}
+		return response, nil
+	}
+	value, err := txn.GetValue(req.Key)
+	if value == nil {
+		response.NotFound = true
+		return response, nil
+	}
+	if err != nil {
+		return response, err
+	}
+	response.Value = value
+	return response, nil
+	return response, nil
 }
 
 func (server *Server) KvPrewrite(_ context.Context, req *kvrpcpb.PrewriteRequest) (*kvrpcpb.PrewriteResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	response := new(kvrpcpb.PrewriteResponse)
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		return response, err
+	}
+	defer reader.Close()
+	txn := mvcc.NewMvccTxn(reader, req.StartVersion)
+	// TODO mutation是什么
+	for _, m := range req.Mutations {
+		// 写冲突1：最近写的已经比你新了
+		write, ts, _ := txn.MostRecentWrite(m.Key)
+		if write != nil && req.StartVersion <= ts {
+			response.Errors = append(response.Errors, &kvrpcpb.KeyError{
+				Conflict: &kvrpcpb.WriteConflict{
+					StartTs:    req.StartVersion,
+					ConflictTs: ts,
+					Key:        m.Key,
+					Primary:    req.PrimaryLock,
+				},
+			})
+			continue
+		}
+		var kind mvcc.WriteKind
+		switch m.Op {
+		case kvrpcpb.Op_Put:
+			kind = mvcc.WriteKindPut
+			txn.PutValue(m.Key, m.Value)
+		case kvrpcpb.Op_Del:
+			kind = mvcc.WriteKindDelete
+			txn.DeleteValue(m.Key)
+		case kvrpcpb.Op_Rollback:
+			kind = mvcc.WriteKindRollback
+			// Used by TinySQL but not TinyKV.
+		case kvrpcpb.Op_Lock:
+		default:
+
+		}
+		// 写冲突2：已经有锁了
+		lock, _ := txn.GetLock(m.Key)
+		if lock != nil && lock.Ts != req.StartVersion {
+			response.Errors = append(response.Errors, &kvrpcpb.KeyError{Locked: &kvrpcpb.LockInfo{
+				PrimaryLock: lock.Primary,
+				LockVersion: lock.Ts,
+				Key:         m.Key,
+				LockTtl:     lock.Ttl,
+			}})
+			return response, err
+		}
+		lock = &mvcc.Lock{
+			Primary: req.PrimaryLock,
+			Ts:      req.GetStartVersion(),
+			Ttl:     req.LockTtl,
+			Kind:    kind,
+		}
+		txn.PutLock(m.Key, lock)
+	}
+	server.storage.Write(req.Context, txn.Writes())
+	return response, nil
 }
 
 func (server *Server) KvCommit(_ context.Context, req *kvrpcpb.CommitRequest) (*kvrpcpb.CommitResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	response := new(kvrpcpb.CommitResponse)
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		return response, err
+	}
+	defer reader.Close()
+	txn := mvcc.NewMvccTxn(reader, req.StartVersion)
+	// TODO mutation是什么
+	server.Latches.WaitForLatches(req.Keys)
+	defer server.Latches.ReleaseLatches(req.Keys)
+	for _, key := range req.Keys {
+		// 应该放提交的版本
+		// 如果没加锁就应该直接返回 后面不执行了
+		lock, _ := txn.GetLock(key)
+		if lock == nil {
+			return response, nil
+		}
+
+		if lock.Ts != txn.StartTS {
+			response.Error = &kvrpcpb.KeyError{Retryable: "true"}
+			return response, nil
+		}
+		txn.PutWrite(key, req.CommitVersion, &mvcc.Write{
+			StartTS: req.StartVersion,
+			Kind:    mvcc.WriteKindPut,
+		})
+		txn.DeleteLock(key)
+	}
+	server.storage.Write(req.Context, txn.Writes())
+	return response, nil
 }
 
 func (server *Server) KvScan(_ context.Context, req *kvrpcpb.ScanRequest) (*kvrpcpb.ScanResponse, error) {
